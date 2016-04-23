7893381b88267ef9b066225326524e07b6ecb72e
Yixing <410517667@qq.com>
2016-04-23 19:40:48 -0400

    Draw
    Put the txt file at the appropriate location, and revise the in-file
    stream path in points_and_lines.cpp
    In txt file, for every 3 lines, the floats are x, y, z coordinates.

---
+++ src/points_and_lines/src/basic_shapes.cpp
@@ -0,0 +1,91 @@
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+
+int main( int argc, char** argv )
+{
+  ros::init(argc, argv, "basic_shapes");
+  ros::NodeHandle n;
+  ros::Rate r(1);
+  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+
+  uint32_t shape = visualization_msgs::Marker::SPHERE;
+
+  while (ros::ok())
+  {
+    visualization_msgs::Marker marker;
+    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+    marker.header.frame_id = "my_frame";
+    marker.header.stamp = ros::Time::now();
+
+    marker.ns = "basic_shapes";
+    marker.id = 0;
+
+    marker.type = shape;
+
+    marker.action = visualization_msgs::Marker::ADD;
+
+    marker.pose.position.x = 0;
+    marker.pose.position.y = 0;
+    marker.pose.position.z = 0;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+// %EndTag(POSE)%
+
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+// %Tag(SCALE)%
+    marker.scale.x = 1.0;
+    marker.scale.y = 1.0;
+    marker.scale.z = 1.0;
+// %EndTag(SCALE)%
+
+    // Set the color -- be sure to set alpha to something non-zero!
+// %Tag(COLOR)%
+    marker.color.r = 0.0f;
+    marker.color.g = 1.0f;
+    marker.color.b = 0.0f;
+    marker.color.a = 1.0;
+// %EndTag(COLOR)%
+
+// %Tag(LIFETIME)%
+    marker.lifetime = ros::Duration();
+// %EndTag(LIFETIME)%
+
+    // Publish the marker
+// %Tag(PUBLISH)%
+    while (marker_pub.getNumSubscribers() < 1)
+    {
+      if (!ros::ok())
+      {
+        return 0;
+      }
+      ROS_WARN_ONCE("Please create a subscriber to the marker");
+      sleep(1);
+    }
+    marker_pub.publish(marker);
+
+
+    // Cycle between different shapes
+
+    switch (shape)
+    {
+    case visualization_msgs::Marker::CUBE:
+      shape = visualization_msgs::Marker::SPHERE;
+      break;
+    case visualization_msgs::Marker::SPHERE:
+      shape = visualization_msgs::Marker::ARROW;
+      break;
+    case visualization_msgs::Marker::ARROW:
+      shape = visualization_msgs::Marker::CYLINDER;
+      break;
+    case visualization_msgs::Marker::CYLINDER:
+      shape = visualization_msgs::Marker::CUBE;
+      break;
+    }
+    
+    sleep(1);
+    // r.sleep();
+  }
+
+}
